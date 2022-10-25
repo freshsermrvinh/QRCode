de75f84bb86c2c47b16fab6f2c5c93169b4163b8
Vinh Phu Pham <vinhpp.tgg@vnpt.vn>
2022-10-26 01:27:57 +0700

    'QR'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'QR';
-  public qrInfo = 'QR';
+  public baseInfo = 'http://localhost:4200/';
+  public qrInfo = this.baseInfo;
   public width = 399;
+  public name ='';
+  public age = 0;
 
-  public change(event: any): void {
+  constructor(route: ActivatedRoute){
+    route.queryParams.subscribe((v) => {
+      console.log("queryParams = ",v);
+      if(v && v.data){
+        const data = v.data;
+        console.log("queryParams = ",data);
+        console.log("name = ",data.name);
+        console.log("age = ",data.age);
+        
+        const obj = JSON.parse(data);
+        console.log("queryParams Oject = ",obj);
+        console.log("name = ",obj.name)
+        console.log("age = ",obj.age)
+
+      }   
+  });
+}
+
+  public ngOnInit(): void{}
+
+  public changeBase(event: any): void {
     console.log('event=', event.target.value);
+    this.baseInfo = event.target.value;
     this.qrInfo = event.target.value;
+    this.combine();
+  }
+  public changeName(event: any): void {
+    this.name = event.target.value;
+    this.combine();
+  }
+  public changeAge(event: any): void {
+    this.age = event.target.value;
+    this.combine();
+  }
+
+  public combine():void{
+    const data = JSON.stringify({name:this.name,age:this.age});
+    this.qrInfo = this.baseInfo +'?data=' + data;
+    console.log('qrInfo',this.qrInfo);
   }
 }
